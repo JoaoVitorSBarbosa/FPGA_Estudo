92c23e0757e6e709dc5bd28545e8df96fb5d3d0d
João Vitor Santos <jvsb2017@gmail.com>
2024-04-12 15:18:36 -0300

    Add pratica 3

--- pratica3/pratica3.v
+++ pratica3/pratica3.v
@@ -1,7 +1,13 @@
 module pratica3 (
-	input wire [9:0] SW
+	input wire [9:0] SW,
+	output wire [6:0]HEX0,
+	output wire [6:0]HEX1
 );
-
-contaUns();
-
+	
+	wire [3:0]c; 
+	contaUns contador (.d(SW), .q(c));
+	SEG7_LUT disp0 (.iDIG((c % 10)), .oSEG(HEX0));
+	SEG7_LUT disp1 (.iDIG((c / 10)), .oSEG(HEX1));
+	
+	
 endmodule
